210a9ebabbb0d6b2da319277351a24725f2d99fc
Zeeshan <equbalzeeshan@gmail.com>
2021-07-04 02:01:37 +0530

    platform_code/helper/model: Move notify() code to platform_code module.
    This commit shifts the notify() function to the platform_code.py module
    in an effort to move all platform dependent code to a single isolated
    module so that other modules are unaware of the underlying platform and
    how they are handled for different platforms. This will also allow us to
    better handle unsupported platforms.
    
    Tests amended.

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -8,7 +8,6 @@ from zulipterminal.helper import (
     get_unused_fence,
     hash_util_decode,
     index_messages,
-    notify,
     notify_if_message_sent_outside_narrow,
     powerset,
 )
@@ -278,60 +277,6 @@ def test_invalid_color_format(mocker, color):
     with pytest.raises(ValueError) as e:
         canon = canonicalize_color(color)
     assert str(e.value) == f'Unknown format for color "{color}"'
-
-
-@pytest.mark.parametrize(
-    "PLATFORM, is_notification_sent",
-    [
-        # PLATFORM: Literal["WSL", "MacOS", "Linux", "unsupported"]
-        pytest.param(
-            "WSL",
-            True,
-            marks=pytest.mark.xfail(reason="WSL notify disabled"),
-        ),
-        ("MacOS", True),
-        ("Linux", True),
-        ("unsupported", False),  # Unsupported OS
-    ],
-)
-def test_notify(mocker, PLATFORM, is_notification_sent):
-    title = "Author"
-    text = "Hello!"
-    mocker.patch(MODULE + ".PLATFORM", PLATFORM)
-    subprocess = mocker.patch(MODULE + ".subprocess")
-    notify(title, text)
-    assert subprocess.run.called == is_notification_sent
-
-
-@pytest.mark.parametrize(
-    "text",
-    ["x", "Spaced text.", "'", '"'],
-    ids=["x", "spaced_text", "single", "double"],
-)
-@pytest.mark.parametrize(
-    "title",
-    ["X", "Spaced title", "'", '"'],
-    ids=["X", "spaced_title", "single", "double"],
-)
-@pytest.mark.parametrize(
-    "PLATFORM, cmd_length",
-    [
-        ("Linux", 4),
-        ("MacOS", 10),
-        pytest.param("WSL", 2, marks=pytest.mark.xfail(reason="WSL notify disabled")),
-    ],
-)
-def test_notify_quotes(monkeypatch, mocker, PLATFORM, cmd_length, title, text):
-    subprocess = mocker.patch(MODULE + ".subprocess")
-    platform = mocker.patch(MODULE + ".PLATFORM", PLATFORM)
-
-    notify(title, text)
-
-    params = subprocess.run.call_args_list
-    assert len(params) == 1  # One external run call
-    assert len(params[0][0][0]) == cmd_length
-
-    # NOTE: If there is a quoting error, we may get a ValueError too
 
 
 @pytest.mark.parametrize(
